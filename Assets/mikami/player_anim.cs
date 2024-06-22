1f6bdb9733b4e5db36970be0811dbbe21b4753e0
Shota-Mikami <shota.mikami.5288@gmail.com>
2024-06-22 13:43:55 +0900

    axe_coll_fixed

--- Assets/mikami/player_anim.cs
+++ Assets/mikami/player_anim.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class player_anim : MonoBehaviour
 {
+    [SerializeField] private BoxCollider axe;
     private Animator anim;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,8 @@ public class player_anim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        axe.isTrigger = anim.GetCurrentAnimatorStateInfo(0).IsName("swing");
+
         if (Input.GetMouseButton(0))
         {
             anim.SetInteger("status_anim", 3);
