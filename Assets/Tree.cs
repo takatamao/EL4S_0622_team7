7b205ece78eeaff43ab97049a01cc7af8bba638f
takatamao <yuzu03133@gmail.com>
2024-06-22 14:19:22 +0900

    木のランダム生成の修正

--- Assets/Tree.cs
+++ Assets/Tree.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Tree : GimmickBase
 {
     [SerializeField] private GameObject _Branch;
-    [SerializeField] private float _instantiateRange = 9;
+   // [SerializeField] private float _instantiateRange = 9;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +21,18 @@ public class Tree : GimmickBase
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Axe"))
+        int x = Random.Range(-30, 30);
+        if (x <= 0) x -= 5;
+        else x += 5;
+
+        int z = Random.Range(-15, 35);
+        if (z <= 0) x -= 5;
+        else z += 5;
+
+        if (other.CompareTag("Axe"))
         {
             Instantiate(_Branch,transform.position,Quaternion.identity);
-            Instantiate(this.gameObject, new Vector3(Random.Range(-_instantiateRange, _instantiateRange), 0.0f, Random.Range(-_instantiateRange, _instantiateRange)), Quaternion.identity);
+            Instantiate(this.gameObject, new Vector3(x, 0.0f, z), Quaternion.identity);
             Destroy(this.gameObject);
         }
     }
